e2277d178e6fd8d7a53cf2b320f8b21a59e9d62a
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-06-25 19:01:46 +0700

    Create authentication.service.ts

---
+++ src/app/features/authentication/services/authentication.service.ts
@@ -0,0 +1,72 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, throwError } from 'rxjs';
+
+import { Credentials } from '../domain/entities/credentials';
+import { HttpResponseEntity } from '../../../core/domain/http-response-entity';
+import { Injectable } from '@angular/core';
+import { LoginDto } from '../domain/dto/login.dto';
+import { RegisterDto } from '../domain/dto/register.dto';
+import { StorageService } from '../../../core/services/storage.service';
+import { environment } from '../../../../environments/environment.development';
+import { handlerHttpError } from '../../../core/utility/http-handle-error';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  endpoint = environment.endpoint;
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly storageService: StorageService
+  ) { }
+
+  login(body: LoginDto): Observable<HttpResponseEntity<Credentials>> {
+    return this.http.post<HttpResponseEntity<Credentials>>(`${this.endpoint}/auth/login`, body).pipe(
+      map((response) => {
+        this.storageService.setAccessToken(response.data.accessToken);
+        this.storageService.setRefreshToken(response.data.refreshToken);
+        return {
+          message: response.message,
+          data: {
+            accessToken: response.data.accessToken,
+            refreshToken: response.data.refreshToken,
+          },
+        };
+      }),
+      catchError((error: HttpErrorResponse) => handlerHttpError(error))
+    );
+  }
+
+  register(body: RegisterDto): Observable<HttpResponseEntity<Credentials>> {
+    return this.http.post<HttpResponseEntity<Credentials>>(`${this.endpoint}/auth/register`, body);
+  }
+
+  logout(accessToken: string): Observable<any> {
+    const endpoint = `${this.endpoint}/auth/logout`;
+    if (!accessToken) {
+      return throwError(() => console.error('Access token not found!'));
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${accessToken}`);
+    return this.http
+      .post(endpoint, {}, { headers: headers })
+      .pipe(map(() => this.storageService.clear()),
+        catchError((error: HttpErrorResponse) => handlerHttpError(error)));
+  }
+
+  generateRefreshToken(refreshToken: string): Observable<HttpResponseEntity<Credentials>> {
+    if (!refreshToken) {
+      return throwError(() => console.error('Refresh token not found!'));
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${refreshToken}`);
+    return this.http.post<HttpResponseEntity<Credentials>>(
+      `${this.endpoint}/auth/refresh`,
+      {},
+      { headers: headers }
+    );
+  }
+
+}
