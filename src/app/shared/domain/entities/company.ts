617f43338fa32128f76557d1a9de2de4c803b873
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-06-26 15:32:07 +0700

    Create company.ts

---
+++ src/app/shared/domain/entities/company.ts
@@ -0,0 +1,13 @@
+export interface Company {
+  id: string
+  name: string
+  logo: string
+  website: string
+  industry: string
+  size: number
+  location: string
+  description: string
+  benefit: string
+  contactInfo: string
+  employerId?: string
+}
