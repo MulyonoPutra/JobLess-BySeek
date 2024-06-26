24f5d169b6fb4f95b9e88925a445441d3093a2ac
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-06-26 15:32:53 +0700

    Update jobs.routes.ts

--- src/app/features/jobs/jobs.routes.ts
+++ src/app/features/jobs/jobs.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const JOBS_ROUTES: Routes = [
 	{
 		path: '',
-		loadComponent: () => import('./pages/jobs/jobs.component').then((a) => a.JobsComponent),
+		loadComponent: () => import('./pages/job-ads/job-ads.component').then((a) => a.JobAdsComponent),
 	},
 	{
 		path: 'results',
