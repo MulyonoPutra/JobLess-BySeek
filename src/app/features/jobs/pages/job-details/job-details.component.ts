76567b2bcc4e8133fcaf5bfa054ae4513142c7e3
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-06-26 15:32:58 +0700

    Update job-details.component.ts

--- src/app/features/jobs/pages/job-details/job-details.component.ts
+++ src/app/features/jobs/pages/job-details/job-details.component.ts
@@ -1,14 +1,53 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component, DestroyRef, type OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { JobAdsService } from '../../services/job-ads.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { JobAds } from '../../domain/entities/job-ads';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RupiahPipe } from '../../../../shared/pipes/rupiah.pipe';
+import { TimeAgoPipe } from '../../../../shared/pipes/time-ago.pipe';
 
 @Component({
 	selector: 'app-job-details',
 	standalone: true,
-	imports: [CommonModule, AngularSvgIconModule],
+  imports: [CommonModule, AngularSvgIconModule, TimeAgoPipe, RupiahPipe],
 	templateUrl: './job-details.component.html',
 	styleUrls: ['./job-details.component.scss'],
+  providers: [JobAdsService]
 })
 export class JobDetailsComponent implements OnInit {
-	ngOnInit(): void {}
+
+  jobId!: string;
+  jobAds!: JobAds;
+
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+    private readonly destroyRef: DestroyRef,
+    private readonly location: Location,
+    private readonly jobAdsService: JobAdsService
+  ) {
+    this.jobId = this.route.snapshot.paramMap.get('id')!;
+  }
+
+	ngOnInit(): void {
+    this.findById()
+  }
+
+  findById(): void {
+    this.jobAdsService.findById(this.jobId).pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe({
+      next: (response: JobAds) => {
+        this.jobAds = response;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+      },
+      complete: () => { },
+    });
+  }
+
 }
