a0ce200035bad651b6b0ce7537737cd1b69c1fe2
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-07-03 18:20:19 +0700

    feat: integration find job ads by company ID

--- src/app/features/companies/pages/job-ads-by-company/job-ads-by-company.component.ts
+++ src/app/features/companies/pages/job-ads-by-company/job-ads-by-company.component.ts
@@ -1,48 +1,62 @@
 import { CommonModule } from '@angular/common';
 import { Component, DestroyRef, type OnInit } from '@angular/core';
-import { CardJobAdsComponent } from '../../../../shared/components/molecules/card-job-ads/card-job-ads.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Company } from '../../../../core/domain/entities/company';
-import { StorageService } from '../../../../core/services/storage.service';
-import { CompanyService } from '../../services/company.service';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { TimeAgoPipe } from '../../../../shared/pipes/time-ago.pipe';
+import { Company } from '../../../../core/domain/entities/company';
+import { JobAds } from '../../../../core/domain/entities/job-ads';
+import { CardJobAdsComponent } from '../../../../shared/components/molecules/card-job-ads/card-job-ads.component';
 import { RupiahPipe } from '../../../../shared/pipes/rupiah.pipe';
+import { TimeAgoPipe } from '../../../../shared/pipes/time-ago.pipe';
+import { CompanyService } from '../../services/company.service';
 
 @Component({
-	selector: 'app-job-ads-by-company',
-	standalone: true,
-	imports: [CommonModule, AngularSvgIconModule, CardJobAdsComponent, TimeAgoPipe, RupiahPipe],
-	templateUrl: './job-ads-by-company.component.html',
-	styleUrls: ['./job-ads-by-company.component.scss'],
+  selector: 'app-job-ads-by-company',
+  standalone: true,
+  imports: [CommonModule, AngularSvgIconModule, CardJobAdsComponent, TimeAgoPipe, RupiahPipe],
+  templateUrl: './job-ads-by-company.component.html',
+  styleUrls: ['./job-ads-by-company.component.scss'],
 })
 export class JobAdsByCompanyComponent implements OnInit {
-	company!: Company;
-	companyId!: string;
-	visible = false;
+  company!: Company;
+  companyId!: string;
+  visible = false;
+  jobAds!: JobAds[];
 
-	constructor(
-		private readonly router: Router,
-		private readonly route: ActivatedRoute,
-		private readonly destroyRef: DestroyRef,
-		private readonly companyService: CompanyService,
-		private readonly storageService: StorageService,
-	) {}
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+    private readonly destroyRef: DestroyRef,
+    private readonly companyService: CompanyService,
+  ) { }
 
-	ngOnInit(): void {
-		this.route.parent?.data.subscribe({
-			next: (response) => {
-				console.log(response);
-				this.company = response['data'];
-			},
-		});
-	}
+  ngOnInit(): void {
+    this.getCompanyId();
+    this.findJobAdsByCompanyId();
+  }
 
-	onDetails(id: string): void {
-		this.router.navigateByUrl(`/`);
-	}
+  getCompanyId() {
+    this.route.parent?.data.subscribe({
+      next: (response) => {
+        this.companyId = response['data'].id;
+      },
+    });
+  }
 
-	onBookmark(): void {}
+  findJobAdsByCompanyId() {
+    this.companyService.findJobAdsByCompanyId(this.companyId).pipe(takeUntilDestroyed(this.destroyRef)
+    ).subscribe({
+      next: (response) => {
+        this.jobAds = response;
+      }
+    })
+  }
 
-	onMark(): void {}
+  onDetails(id: string): void {
+    this.router.navigateByUrl(`/jobs/details/${id}`);
+  }
+
+  onBookmark(): void { }
+
+  onMark(): void { }
 }
